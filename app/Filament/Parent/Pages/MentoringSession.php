b8765be9055df90159ffe13f9ad0d1f28f50300c
Glan Oka Parisudha <glanokaparisudha@gmail.com>
2026-05-07 22:43:02 +0800

    feat: mentoring session page

---
+++ app/Filament/Parent/Pages/MentoringSession.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Parent\Pages;
+
+use Filament\Pages\Page;
+
+class MentoringSession extends Page
+{
+    protected string $view = 'filament.parent.pages.mentoring-session';
+}
